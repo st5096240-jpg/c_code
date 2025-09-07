c3c9e49966e78df2a6e0206b436af2764966d47e
st5096240-jpg <st5096240@gmail.com>
2025-09-07 10:48:36 +0530

    Updated code

---
+++ C/prac3.c/PorF.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int mark;
+    printf("enter the marks:");
+    scanf("%d",&mark);
+    if (mark>30)
+    {
+        printf("PASS!");
+    }
+    else{
+        printf("FAIL!");
+    }
+
+
+}
